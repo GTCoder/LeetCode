f04c05fec8bc729f723646d538ef66721b98164f
Dongye Meng <dmeng@cs.unc.edu>
2014-08-06 21:34:22 -0400

    update

--- RemoveDuplicatesfromSortedList.java
+++ RemoveDuplicatesfromSortedList.java
@@ -1,42 +1,46 @@
 /**
- * Three cases:
- * 1) If the current is 0, swap it to left. Move BOTH left and current by 1;
- * 2) If the current is 1, no swap needed. Move ONLY current by 1;
- * 3) If the current is 2, swap it to right. Move ONLY right by 1!!! This is because the right element swapped to current may be 0, so we may need to swap it further to left, so we need one more iteration to handle it, and must not move current at this iteration.
- **/
+ * 1. Add a dummy head
+ * 2. Use two while loops
+ * 3. Remember this piece of code:
+ *         prev.next = cur;
+ *         cur = cur.next;
+ *         prev = prev.next;
+ *    Note that prev is touched TWICE here!
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
-    public void sortColors(int[] A) {
-        if (A == null || A.length == 0) {
-            return;
+    public ListNode deleteDuplicates(ListNode head) {
+        // Validate input
+        if (head == null || head.next == null) {
+            return head;
         }
         
-        int left = 0;
-        int right = A.length - 1;
-        int p = 0;
+        // Add a dummy node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
         
-        while (p <= right) {
-            if (A[p] < 1) {
-                swap(A, p, left);
-                left++;
-                p++;
+        ListNode prev = dummy;
+        ListNode cur = head;
+        
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
             }
-            else if (A[p] == 1) {
-                p++;
-            }
-            else {
-                swap(A, p, right);
-                right--;
-            }
-        }
-    }
-    
-    private void swap(int[] A, int a, int b) {
-        if (A == null || A.length == 0 || a < 0 || b >= A.length) {
-            return;
+            prev.next = cur;
+            cur = cur.next;
+            prev = prev.next;
         }
         
-        int temp = A[a];
-        A[a] = A[b];
-        A[b] = temp;
+        return dummy.next;
     }
 }
