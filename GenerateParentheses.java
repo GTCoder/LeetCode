469b464b742b87714893f78348a9060080e3cba4
Dongye Meng <dmeng@cs.unc.edu>
2014-08-06 20:59:04 -0400

    add comments

--- GenerateParentheses.java
+++ GenerateParentheses.java
@@ -1,16 +1,18 @@
 /**
  * This problem is equivalent to the problem of finding the total number of
  * different paths from the upper-left corner to the bottom-right corner in the
- * upper-right triangle of a square.
+ * upper-right triangle of a square
  * 
- * Use recursion to solve it.
+ * How to solve:
+ * 1. Use recursion to solve it
+ * 2. Before and after each recursive call, add/delete parenthesis explicitly
  * 
  * Note: 
- * 1. To add a char to a StringBuilder, use append(), not add(); 
- * 2. To delete last char, use deleteCharAt(), not delete(); 
+ * 1. To add a char to a StringBuilder, use append(), not add()
+ * 2. To delete last char, use deleteCharAt(), not delete()
  * 3. Use StringBuilder instead of string concat, because 
- *     1) it is more efficient in terms of time and space; 
- *     2) the code is more understandable.
+ *     1) it is more efficient in terms of time and space
+ *     2) the code is more understandable
  */
 public class Solution {
 	public List<String> generateParenthesis(int n) {
